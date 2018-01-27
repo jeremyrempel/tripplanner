42be48c0b0e075120debdcf55b7864d97da44c7e
Jeremy Rempel <jr@idreamincolour.com>
2018-01-27 16:46:09 -0500

    Implement search provider with sample data

---
+++ app/src/main/java/com/travelsloth/tripplanner/SearchProvider.kt
@@ -0,0 +1,51 @@
+package com.travelsloth.tripplanner
+
+import android.app.SearchManager
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.MatrixCursor
+import android.net.Uri
+import android.provider.BaseColumns
+import timber.log.Timber
+
+/**
+ * Created by jrempel on 1/27/18.
+ */
+class SearchProvider : ContentProvider() {
+
+    override fun insert(uri: Uri?, values: ContentValues?): Uri {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    // uri: content://net.holidayweather.search.provider/search_suggest_query/nnn?limit=50
+    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor {
+        Timber.d("query: uri: %s, projection: %s, selection: %s, selectionArgs: %s, sortOrder: %s", uri, projection, selection, selectionArgs, sortOrder)
+
+        val query = if (uri?.pathSegments?.size!! > 1) uri?.lastPathSegment?.toLowerCase() else ""
+
+        Timber.d("query: %s", query)
+
+        val cursor = MatrixCursor(arrayOf(BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1))
+        cursor.addRow(arrayOf("1", "New York"))
+        cursor.addRow(arrayOf("2", "Vancouver"))
+
+        return cursor
+    }
+
+    override fun onCreate(): Boolean {
+        return true
+    }
+
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getType(uri: Uri?): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
